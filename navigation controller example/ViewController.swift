711b0d87b6c1b2c1d1eb0a27b91fb59eec5af845
student <student@productservice.ttknn.net>
2018-02-01 15:16:19 +0300

    Initial Commit

---
+++ navigation controller example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  navigation controller example
+//
+//  Created by student on 01.02.2018.
+//  Copyright © 2018 Eldar Akavov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
